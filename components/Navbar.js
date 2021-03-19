f44d9082e6b36890b7cd9087ba4267b727aaa535
KuanLunTseng <tsengku@oregonstate.edu>
2021-03-19 10:13:05 -0700

    Log out

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,13 +8,14 @@ import { slide as Menu } from 'react-burger-menu';
 
 import colors from '../styles/colors';
 import breakpoints from '../styles/breakpoints';
+import Cookies from 'universal-cookie'
 
 //Search bar for the Navbar
 function SearchBar() {
-    const [ searchString, setSearchString] = useState('');
-
-    //Styles for the search bar
-    const searchBarStyles = css`
+  const [searchString, setSearchString] = useState('');
+  
+  //Styles for the search bar
+  const searchBarStyles = css`
         display: inline-block;
         border: 2px solid darkgray;
         height: 50px;
@@ -61,31 +62,31 @@ function SearchBar() {
         }
     `;
 
-    return (
-        <div css={searchBarStyles} >
-            <form onSubmit={(e) => {
-                //TODO: Hook this up to send query string parameters to Search page
-                alert("Searched for: " + searchString);
-                setSearchString('');
-                e.preventDefault();
-            }}>
-                <input
-                    value={searchString}
-                    placeholder='Search for a song or artist...'
-                    onChange={(e) => setSearchString(e.target.value)}
-                />
-                <button type="submit">
-                    <FaSearch />
-                </button>
-            </form>
-        </div>
-    )
+  return (
+    <div css={searchBarStyles} >
+      <form onSubmit={(e) => {
+        //TODO: Hook this up to send query string parameters to Search page
+        alert("Searched for: " + searchString);
+        setSearchString('');
+        e.preventDefault();
+      }}>
+        <input
+          value={searchString}
+          placeholder='Search for a song or artist...'
+          onChange={(e) => setSearchString(e.target.value)}
+        />
+        <button type="submit">
+          <FaSearch />
+        </button>
+      </form>
+    </div>
+  )
 }
 function Navbar() {
-    const [showHamnav, setShowHamnav] = useState(false);
+  const [showHamnav, setShowHamnav] = useState(false);
 
-    //Styles for the entire navbar
-    const navbarStyles = css`
+  //Styles for the entire navbar
+  const navbarStyles = css`
         width: 100%;
         height: 80px;
         top: 0;
@@ -98,8 +99,8 @@ function Navbar() {
         border-radius: 4px;
     `;
 
-    //Parent styles for all links on the Navbar
-    const navItemStyles = css`
+  //Parent styles for all links on the Navbar
+  const navItemStyles = css`
         padding-top: 25px;
         height: calc(100% - 25px); //subtract the padding;
         width: 8%;
@@ -118,8 +119,8 @@ function Navbar() {
         }
     `;
 
-    //Styles for the site title on the Navbar
-    const siteTitleStyles = css`
+  //Styles for the site title on the Navbar
+  const siteTitleStyles = css`
         ${navItemStyles}
 
         width: 15%;
@@ -130,8 +131,8 @@ function Navbar() {
         color: ${colors.darkGreen};
     `;
 
-    //Styles for the logout button
-    const logOutButtonStyles = css`
+  //Styles for the logout button
+  const logOutButtonStyles = css`
         ${navItemStyles};
         float: right;
         padding: 30px 10px 0 0;
@@ -140,101 +141,104 @@ function Navbar() {
         height: calc(100% - 30px); //subtract the padding
     `;
 
-    const burgerMenuItemStyles = css`
+  const burgerMenuItemStyles = css`
         :hover {
             background-color: ${colors.hoverBlack};
         }
     `;
-    var burgerMenuStyles = {
-        bmBurgerButton: {
-            position: 'fixed',
-            width: '36px',
-            height: '30px',
-            right: '36px',
-            top: '25px'
-        },
-        bmBurgerBars: {
-            background: 'white',
-        },
-        bmCrossButton: {
-            height: '40px',
-            width: '40px',
-        },
-        bmCross: {
-            background: '#bdc3c7',
-            height: '30px',
-            right: '10px'
-        },
-        bmMenuWrap: {
-            position: 'fixed',
-            top: 0,
-            bottom: 0
-        },
-        bmMenu: {
-            background: `${colors.black}`,
-            padding: '80px 0',
-            fontSize: '1.15em'
-        },
-        bmMorphShape: {
-            fill: '#373a47'
-        },
-        bmItemList: {
-            color: '#b8b7ad',
-            overflow: 'none',
-            height: '20%'
-        },
-        bmItem: {
-            display: 'inline-block',
-            width: '100%',
-            textDecoration: 'none',
-            textAlign: 'center',
-            color: 'white',
-            padding: '10px 0px',
-            fontSize: '25px',
-        },
-        bmOverlay: {
-            background: 'rgba(0, 0, 0, 0.3)',
-            top: 0,
-            left: 0
-        }
+  var burgerMenuStyles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '36px',
+      height: '30px',
+      right: '36px',
+      top: '25px'
+    },
+    bmBurgerBars: {
+      background: 'white',
+    },
+    bmCrossButton: {
+      height: '40px',
+      width: '40px',
+    },
+    bmCross: {
+      background: '#bdc3c7',
+      height: '30px',
+      right: '10px'
+    },
+    bmMenuWrap: {
+      position: 'fixed',
+      top: 0,
+      bottom: 0
+    },
+    bmMenu: {
+      background: `${colors.black}`,
+      padding: '80px 0',
+      fontSize: '1.15em'
+    },
+    bmMorphShape: {
+      fill: '#373a47'
+    },
+    bmItemList: {
+      color: '#b8b7ad',
+      overflow: 'none',
+      height: '20%'
+    },
+    bmItem: {
+      display: 'inline-block',
+      width: '100%',
+      textDecoration: 'none',
+      textAlign: 'center',
+      color: 'white',
+      padding: '10px 0px',
+      fontSize: '25px',
+    },
+    bmOverlay: {
+      background: 'rgba(0, 0, 0, 0.3)',
+      top: 0,
+      left: 0
     }
+  }
+  const cookies = new Cookies
+  const updateMedia = () => {
+    setShowHamnav(window.innerWidth <= breakpoints.lg);
+  };
 
-    const updateMedia = () => {
-        setShowHamnav(window.innerWidth <= breakpoints.lg);
-    };
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
 
-    useEffect(() => {
-        window.addEventListener("resize", updateMedia);
-        return () => window.removeEventListener("resize", updateMedia);
-    });
+  useEffect(() => {
+    setShowHamnav(window.innerWidth <= breakpoints.lg);
+  }, []);
 
-    useEffect(() => {
-        setShowHamnav(window.innerWidth <= breakpoints.lg);
-    }, []);
-
-    return(
+  return (
+    <>
+      {showHamnav ?
         <>
-            {showHamnav ?
-                <>
-                    <div css={navbarStyles}>
-                        <SearchBar />
-                    </div>
-                    <Menu right styles={burgerMenuStyles}>
-                        <a href="/" css={burgerMenuItemStyles}>Home</a>
-                        <a href="about" css={burgerMenuItemStyles}>About</a>
-                        <a href="/" css={burgerMenuItemStyles}>Log Out</a>
-                    </Menu>
-                </> :
-                <div css={navbarStyles}>
-                    <a css={siteTitleStyles} href="/">SpotifyEZ</a>
-                    <a css={navItemStyles} href="/">Home</a>
-                    <a css={navItemStyles} href="about">About</a>
-                    <SearchBar />
-                    <a css={logOutButtonStyles} href="/">Log Out</a>
-                </div>
-            }
-        </>
-    );
+          <div css={navbarStyles}>
+            <SearchBar />
+          </div>
+          <Menu right styles={burgerMenuStyles}>
+            <a href="/" css={burgerMenuItemStyles}>Home</a>
+            <a href="about" css={burgerMenuItemStyles}>About</a>
+            <a href="/" css={burgerMenuItemStyles}>Log Out</a>
+          </Menu>
+        </> :
+        <div css={navbarStyles}>
+          <a css={siteTitleStyles} href="/">SpotifyEZ</a>
+          <a css={navItemStyles} href="/">Home</a>
+          <a css={navItemStyles} href="about">About</a>
+          <SearchBar />
+          <a onClick={() => {
+            cookies.remove('ACESS_TOKEN')
+            cookies.remove('IS_AUTHED')
+          }} css={logOutButtonStyles} href="/">Log Out</a>
+        </div>
+      }
+    </>
+  );
 }
 
 export default Navbar;
