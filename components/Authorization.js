926488f3c25a7315f7720949599e99572975bff4
KuanLunTseng <tsengku@oregonstate.edu>
2021-03-19 09:05:04 -0700

    cookie set up

--- components/Authorization.js
+++ components/Authorization.js
@@ -2,9 +2,10 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setAcessToken, setExpiresIn, setIsAuthed } from '../store/actions/authAction'
 import { getAuthorizeHref, getHashParams, removeHashParamsFromUrl } from '../auth/Auth'
+import Cookies from 'universal-cookie'
 
 const Authorization = () => {
-
+  const cookies = new Cookies
   const dispatch = useDispatch()
   const { accessToken, isAuthed } = useSelector(state => state.auth)
   //console.log('Access Token === ', accessToken)
@@ -15,9 +16,23 @@ const Authorization = () => {
     const accessToken = params.access_token
     const expiresIn = params.expires_in
 
+    console.log('COOOOOOOOKIE', document.cookie)
+
     if (accessToken) {
-      dispatch(setAcessToken(accessToken))
-      dispatch(setExpiresIn(expiresIn))
+
+      document.cookie = accessToken
+      // console.log('COOOOOOOOKIE', document.cookie)
+
+      cookies.set('ACESS_TOKEN', accessToken, { path: '/' })
+      cookies.set('IS_AUTHED', true, { path: '/' })
+      // console.log('COOOOOOOOOOKIE', cookies.get('ACESS_TOKEN'))
+      // console.log('AUUUUUUUUUUTH', cookies.get('IS_AUTHED'))
+      if (document.cookie !== undefined) {
+        dispatch(setAcessToken(document.cookie))
+      } else {
+        dispatch(setAcessToken(accessToken))
+      }
+      //dispatch(setExpiresIn(expiresIn))
       dispatch(setIsAuthed(true))
     }
     removeHashParamsFromUrl()
@@ -26,13 +41,13 @@ const Authorization = () => {
   return (
     <div>
       {/* { isAuthed === false && */}
-        <button
-          onClick={() => {
-            window.location = getAuthorizeHref()
-          }}>
-          Authentication
-        </button>
-      {/* } */}
+      <button
+        onClick={() => {
+          window.location = getAuthorizeHref()
+        }}>
+        Authentication
+      </button>
+
       { isAuthed === true &&
         <h6>Acess Token :{accessToken}</h6>
       }
