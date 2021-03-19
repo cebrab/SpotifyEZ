bb3ed8cc7c40aaefcca01e67678db050276d4fe1
KuanLunTseng <tsengku@oregonstate.edu>
2021-03-19 13:39:57 -0700

    actions

--- store/actions/playlistAction.js
+++ store/actions/playlistAction.js
@@ -37,3 +37,39 @@ export const clearPlaylistById = () => dispatch => {
     type: types.CLEAR_PLAYLIST_BY_ID
   })
 }
+
+// Example
+// axios({
+//   method: 'post',
+//   url: baseUrl + 'applications/' + appName + '/dataexport/plantypes' + plan,
+//   headers: {}, 
+//   data: {
+//     foo: 'bar', // This is the body part
+//   }
+// })
+
+// {
+//   "tracks": [
+//     { "uri": "spotify:track:4iV5W9uYEdYUVa79Axb7Rh" },
+//     { "uri": "spotify:track:1301WleyT98MSxVHPZCA6M" }
+//   ]
+// }
+
+export const removeItemsFromPlaylist = (accessToken, playlistId, body) => async dispatch => {
+  const options = {
+    url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + accessToken,
+      'Content-Type': 'application/json',
+    },
+    data: { body }
+  }
+  const res = await axios(options)
+  dispatch({
+    type: types.REMOVE_ITEMS_FROM_PLAYLIST
+  })
+}
+
+
